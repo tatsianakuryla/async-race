eaa059a12bdc5076b853e99995b43b4fa8cac14c
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-03-26 21:00:40 +0100

    chore: add editorconfig and gitattributes for LF consistency

--- async-race/src/api/api-types.ts
+++ async-race/src/api/api-types.ts
@@ -3,24 +3,24 @@ export const BASE_URL = 'http://localhost:3000';
 type Car = {
   name: string;
   color: string;
-}
+};
 
 export type CarWithId = Car & { id: number };
 
 export type GetCarsResponse = {
   cars: CarWithId[];
   totalCount: number;
-}
+};
 
 export type Winner = {
   id: number;
   wins: number;
   time: number;
-}
+};
 
 export type GetResponse<T> = {
   results: T[];
   totalCount: number;
-}
+};
 
 export type ViewsTypes = 'garage' | 'winners';
